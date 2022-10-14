5a864f54523d18daf2c71d81eada62946a64c897
Abhay Sharma <abhaysharmajr@gmail.com>
2022-10-14 15:40:04 +0530

    movie-container

--- src/Movie.js
+++ src/Movie.js
@@ -1,15 +1,15 @@
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
-import { Counter } from "./Counter";
+// import { Counter } from "./Counter";
 import IconButton from "@mui/material/IconButton";
 import InfoIcon from "@mui/icons-material/Info";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
-import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardActions from '@mui/material/CardActions';
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
 
-export function Movie({ movie, id,deleteButton,EditButton }) {
+export function Movie({ movie, id, deleteButton, EditButton }) {
   const styles = {
     color: movie.rating >= 8 ? "green" : "red",
   };
@@ -29,27 +29,33 @@ export function Movie({ movie, id,deleteButton,EditButton }) {
           ></img>
           <CardContent>
             <div className="movie-specs">
-              <h3>{movie.name}
+              <h3>
+                {movie.name}
                 <IconButton aria-label="info" color="primary">
                   <InfoIcon onClick={() => navigate(`/movies${id}`)} />
-                </IconButton></h3>
+                </IconButton>
+              </h3>
               <p style={styles}>{movie.rating}⭐</p>
             </div>
             {show ? <p className="movie-summary">{movie.summary}</p> : null}
             <div className="like-dislike">
               <CardActions>
-                <Counter /> {deleteButton}{EditButton}
+                <div className="movie-buttons">
+                  <div>{deleteButton}</div>
+                  <div>{EditButton}</div>
+                </div>
               </CardActions>
-
             </div>
-            <IconButton color="primary" aria-label="toggle" onClick={() => setShow(!show)}>
+            <IconButton
+              color="primary"
+              aria-label="toggle"
+              onClick={() => setShow(!show)}
+            >
               {show ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
             </IconButton>
           </CardContent>
-
         </div>
       </Card>
-
     </div>
   );
 }
