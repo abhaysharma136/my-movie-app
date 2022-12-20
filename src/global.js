092e9cfc876f0a9720909b522853adb54de41ed3
Abhay Sharma <abhaysharmajr@gmail.com>
2022-12-20 13:04:22 +0530

    change global Api

--- src/global.js
+++ src/global.js
@@ -1,4 +1,4 @@
 
 // export const API=`https://627dfcd0b75a25d3f3af4996.mockapi.io`;
-export const API=`https://abhay-new-node-app.herokuapp.com`;
+export const API=`https://guvi-node-app-new.vercel.app`;
 //  export const API=`http://localhost:4000`;
